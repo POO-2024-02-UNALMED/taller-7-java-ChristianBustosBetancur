86d31b2f84198d98ef9d2fb18a28050f9e858b26
Christian Bustos Betancur <111025495+ChristianBustosBetancur@users.noreply.github.com>
2024-12-27 20:48:30 +0000

    Prueba 1

---
+++ src/main/java/comunicacion/Fabula.java
@@ -0,0 +1,60 @@
+package comunicacion;
+
+public class Fabula extends Escrito{
+	private String ensenanzas;
+	private String interpretacion;
+
+	
+	
+	
+	
+
+	public Fabula(String origen, String titulo, String autor, int paginas, String ensenanzas, String interpretacion) {
+		super(origen, titulo, autor, paginas);
+		this.ensenanzas = ensenanzas;
+		this.interpretacion = interpretacion;
+	}
+
+
+	
+	
+	
+	
+	
+	public String getEnsenanzas() {
+		return ensenanzas;
+	}
+	public void setEnsenanzas(String ensenanzas) {
+		this.ensenanzas = ensenanzas;
+	}
+	
+	public String getInterpretacion() {
+		return interpretacion;
+	}
+	public void setInterpretacion(String interpretacion) {
+		this.interpretacion = interpretacion;
+	}
+	
+	
+	
+	int palabrasTotales(int palabrasPagina) {
+	    return this.getPaginas() * palabrasPagina;
+	}
+
+
+	String interpretacion() {
+
+		return interpretacion;
+	}
+
+
+	public String toString() {
+	    return String.format("%s\n%s\n%s\n%d\n%s", 
+	        this.getOrigen(), 
+	        this.getTitulo(), 
+	        this.getAutor(), 
+	        this.getPaginas(), 
+	        this.getEnsenanzas());
+	}
+
+}
